2c15639fd76904cc29e8f1b4869e575d4d71ef28
metwse <108795071+metwse@users.noreply.github.com>
2022-08-23 20:24:12 +0300

    v0.01

---
+++ js/anonimsin.js
@@ -0,0 +1,33 @@
+﻿const DATABASE_VERSION = 1
+const URL_BACKEND = 'http://api.anonimsin/utb'
+const URL_WS = 'ws://api.anonimsin/utb'
+
+const db = new Dexie('AnonimsinDB')
+db.version(DATABASE_VERSION).stores({
+    users: '&id,name,avatar',
+    channels: '&id,name,icon',
+    messages: '&id,channel_id,user_id,content'
+})
+
+class Session {
+    constructor(token, user) {
+        this.onunexpectederror
+        this.onservererror
+        this.token = token, this.user = user
+    }
+    async request(path, options) {
+        return new Promise(async resolve => {
+            let _options = { ...options }
+            if (options?.body) _options.body = JSON.stringify(options.body), _options.method = !_options.method ? 'POST' : _options.method
+            if (this.token) _options.headers = { ...options?.headers, token: this.token }
+            let raw, ok, res = await fetch(path, _options).catch(err => { if (this.onunexpectederror) this.onconnectionerror(err); resolve([false, false, false]) }).then(res => {
+                ok = res.ok, raw = res; try { return res.json() } catch (err) { if (this.onservererror) this.onservererror(err); resolve([false, false, false]) }
+            }).then(json => [json, ok, raw]).catch(err => { if (this.onunexpectederror) this.onconnectionerror(err); resolve([false, false, false]) })
+            if (res?.[2].status == 429 && options?.retry != false) setTimeout(async () => resolve(await this.request(path, options)), (parseInt(res[2].headers.get('RateLimit-Reset'))) * 1000)
+            else resolve(res)
+        })
+    }
+    async login() {
+
+    }
+}
--- js/index.js
+++ js/index.js
@@ -2,17 +2,35 @@
 
 const encryptAES = (text, key) => CryptoJS.AES.encrypt(text, key).toString()
 const decryptAES = (encryptedBase64, key) => CryptoJS.AES.decrypt(encryptedBase64, key).toString(CryptoJS.enc.Utf8)
+const sha256 = (text) => CryptoJS.SHA256(text).toString()
 
-
-
+// #region -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- HOMEPAGE
 d.querySelector('#main .get-started button').onclick = () => {
-    d.querySelector('#main').style = 'opacity: 0; transform: translateY(10px)'
-    setTimeout(() => { d.querySelector('#main').style.display = 'none' }, 300)
+    var [startButton, enterUsername] = [d.querySelector('#main .get-started .start'), d.querySelector('#main .get-started .enter-username')],
+        usernameInput = enterUsername.querySelector('input'), ok = enterUsername.querySelector('.ok')
+    startButton.remove()
+    enterUsername.style.display = 'flex'
+    setTimeout(() => enterUsername.style.width = 'min(20em,100%)', 1)
+    usernameInput.onkeypress = ({ key }) => { if (key == 'Enter') ok.click() }
+    usernameInput.oninput = ({ target }) => { target.value = target.value.substring(0, 32) }
+    ok.onclick = () => {
+        localStorage.setItem('token', sha256(crypto.getRandomValues(new Uint8Array(1024)).toString()))
+        localStorage.setItem('user', JSON.stringify({ name: usernameInput.value, avatar: null }))
+        d.getElementById('app').style.display = 'flex'
+        setTimeout(() => d.getElementById('app').style = 'display: flex; opacity: 1; transform: translateY(0) scale(100%)', 1)
+        setTimeout(() => d.getElementById('main').style.display = 'none' , 300)
+    }
 }
+// #endregion  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-w.onload = () => {
+
+w.onload = async () => {
+    var token = localStorage.getItem('token'),
+        user = localStorage.getItem('user')
+
+    d.getElementById('main').style.display = 'block'
     d.querySelector('#initial-loading').style = 'opacity: 0'
     d.querySelector('#initial-loading span').style = 'transform: translateY(.25em)'
     d.querySelector('#initial-loading .loader').style = 'width: 100px'
-    setTimeout(() => { d.querySelector('#initial-loading').remove() }, 300)
+    setTimeout(() => d.querySelector('#initial-loading').remove(), 300)
 }
