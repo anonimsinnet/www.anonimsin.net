073d2f9d22373a6cf9a1bafb2008adbf75543d5e
metwse <108795071+metwse@users.noreply.github.com>
2022-08-25 03:35:01 +0300

    v0.03

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const sha256 = (text) => CryptoJS.SHA256(text).toString()
 d.querySelector('#main .get-started button').onclick = () => {
     var [startButton, enterUsername] = [d.querySelector('#main .get-started .start'), d.querySelector('#main .get-started .enter-username')],
         usernameInput = enterUsername.querySelector('input'), ok = enterUsername.querySelector('.ok')
-    startButton.remove()
+    startButton.style.display = 'none'
     enterUsername.style.display = 'flex'; enterUsername.querySelector('input').focus()
     setTimeout(() => enterUsername.style.width = 'min(20em,100%)', 1)
     usernameInput.onkeypress = ({ key }) => { if (key == 'Enter') ok.click() }
@@ -28,41 +28,129 @@ d.querySelector('#main .get-started button').onclick = () => {
 // #region -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- APP 
 /*
 app
-    channels
-        nav
-            menu
-            chat
-        list
-        search
-    content
+    aside
+        main
+            nav
+                menu
+                chat
+            list
+            search
+        settings
+    main
 */
-app = d.getElementById('app')
-Object.assign(app, { channels: app.querySelector('aside:first-of-type'), content: app.querySelector('main:first-of-type') })
-Object.assign(app.channels, { nav: app.channels.querySelector('nav'), list: app.channels.querySelector('.list'), search: app.channels.querySelector('.search') })
-Object.assign(app.channels.nav, { menu: app.channels.nav.querySelector('.button-menu'), chat: app.channels.nav.querySelector('.button-chat') })
 
-//menu popup
-app.channels.nav.menu.querySelector('span').onclick = ({ target }) => {
-    var popup = app.channels.nav.menu.querySelector('.popup div'),
+/* app */
+/* */ app = d.getElementById('app')
+/* --- */
+    /* app aside,main */
+    /* */ Object.assign(app, { aside: app.querySelector('aside'), main: app.querySelector('main') })
+    /* -------------- */
+        /* app aside main,settings */
+        /* */ Object.assign(app.aside, { main: app.aside.querySelector('.main'), settings: app.aside.querySelector('.settings') })
+        /* ----------------------- */
+            /* app aside settings new-page,old-page*/
+            /* */ Object.assign(app.aside.settings, { newPage: app.aside.querySelector('span > .page-name > .new'), oldPage: app.aside.querySelector('span > .page-name > .old') })
+            /* ----------------------- */
+            /* app aside main nav,list,search */
+            /* */ Object.assign(app.aside.main, { nav: app.aside.querySelector('nav'), list: app.aside.querySelector('.list'), search: app.aside.querySelector('.search') })
+            /* ------------------------------ */
+                /* app aside main nav menu,chat  */
+                /* */ Object.assign(app.aside.main.nav, { menu: app.aside.main.nav.querySelector('.button-menu'), chat: app.aside.main.nav.querySelector('.button-chat') })
+                /* ----------------------------- */
+app.focused = 'none'
+app.busy = false
+app.busyFor = (ms) => { app.busy = true; setTimeout(() => app.busy = false, ms) }
+
+//aside main - menu popup
+app.aside.main.nav.menu.querySelector('span').onclick = ({ target }) => {
+    var popup = app.aside.main.nav.menu.querySelector('.popup div'),
         onfocusout = (e) => {
-            if (!([popup, app.channels.nav.menu]).includes(e?.target.closest('div'))) {
+            if (!([popup, app.aside.main.nav.menu]).includes(e?.target.closest('div'))) {
                 popup.style = 'transform: scale(0.5); opacity: 0', target.closest('span').style.filter = ''; d.removeEventListener('mousedown', onfocusout, true) }
         }
     target.closest('span').style.filter = 'brightness(140%)'
     if (!parseInt(popup.style.opacity)) { popup.style = 'transform: scale(1); opacity: 1'; d.addEventListener('mousedown', onfocusout, true) }
     else onfocusout()
-    
+}
+
+//aside page
+app.aside.page = { /* general */
+    history: [],
+    _open(name) {
+        if (app.busy) return; app.busyFor(300)
+        if (!name) return; app.aside[name].style.display = 'flex'
+        app.focused = ['settings'].includes(name) ? name : none
+        setTimeout(() => app.aside[name].style = 'display: flex; transform: none; opacity: 1', 1)
+    },
+    open(name) {
+        this._open(name); this.history.push(name); window.history.pushState(null, d.title, w.location.pathname)
+    },
+    pop() {
+        var _pop = this.history.pop(), _new = this.history.slice(-1)[0]; if (!_pop || _pop == _new) return
+        app.aside[_pop].style = 'display: flex'; setTimeout(() => app.aside[_pop].style = '', 300); this._open(_new)
+    }
+}
+app.aside.settings.page = { /* settings */
+    history: [],
+    open(name) {
+        if (app.busy) return
+        var _current = this.history.slice(-1)[0]; _current = app.aside.settings.querySelector('.' + (_current ? _current : 'main')),
+            _new = app.aside.settings.querySelector('.' + name)
+        _current.style.transform = 'translateX(-100%)', _new.style = 'display: flex'
+        app.aside.settings.newPage.innerText = _new.getAttribute('data-name'), app.aside.settings.oldPage.innerText = _current.getAttribute('data-name')
+        app.aside.settings.oldPage.style = '', app.aside.settings.newPage.style = 'transform: translateY(100%)'
+        setTimeout(() => {
+            _new.style = 'display: flex; transform: none'
+            app.aside.settings.oldPage.style = 'transform: translateY(-100%); transition: .3s', app.aside.settings.newPage.style = 'transform: none; transition: .3s'
+        }, 1)
+        this.history.push(name); app.busyFor(300)
+    },
+    pop() {
+        if (app.busy) return
+        var [_current, _new] = [undefined, ...this.history.slice(-2)].reverse().slice(0, 2)
+        if (!_current) { app.focused = 'none'; return app.aside.page.pop() }
+        _new = app.aside.settings.querySelector('.' + (_new ? _new : 'main')), _current = app.aside.settings.querySelector('.' + _current)
+        _current.style.transform = 'translateX(100%)', _new.style = 'display: flex'
+        app.aside.settings.newPage.innerText = _new.getAttribute('data-name'), app.aside.settings.oldPage.innerText = _current.getAttribute('data-name')
+        app.aside.settings.oldPage.style = '', app.aside.settings.newPage.style = 'transform: translateY(-100%)'
+        setTimeout(() => {
+            _new.style = 'display: flex; transform: none'
+            app.aside.settings.oldPage.style = 'transform: translateY(100%); transition: .3s', app.aside.settings.newPage.style = 'transform: none; transition: .3s'
+        }, 1)
+        this.history.pop(); app.busyFor(300)
+    }
 }
 // #endregion  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 
+const logout = () => {
+    localStorage.clear()
+    d.querySelector('#main').style.display = 'block'
+    d.querySelector('#main .get-started .start').style.display = 'block'
+    d.querySelector('#main .get-started .enter-username').style = 'display: none'
+    app.style = 'display: flex; opacity: 0; transform: translateY(16px)'
+    setTimeout(() => app.style = '', 300)
+}
+
+
+w.onpopstate = (e) => {
+    switch (app.focused) {
+        case 'settings': app.aside.settings.page.pop(); break
+        default: app.aside.page.pop()
+    }
+    e.preventDefault()
+}
 w.onload = async () => {
     var token = localStorage.getItem('token'),
         user = localStorage.getItem('user')
 
-    d.getElementById('main').style.display = 'block'
     d.querySelector('#initial-loading').style = 'opacity: 0'
     d.querySelector('#initial-loading span').style = 'transform: translateY(.25em)'
     d.querySelector('#initial-loading .loader').style = 'width: 100px'
     setTimeout(() => d.querySelector('#initial-loading').remove(), 300)
+
+    if (token) {
+        d.getElementById('main').style.display = 'none'; d.getElementById('app').style.display = 'flex'
+        setTimeout(() => d.getElementById('app').style = 'display: flex; opacity: 1; transform: translateY(0) scale(100%)', 1)
+    }
 }
